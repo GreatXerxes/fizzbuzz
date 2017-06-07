7a1a392682df017687f7ae2071d378eb1bc8b0c8
GreatXerxes <GreatXerxes@users.noreply.github.com>
2017-06-07 16:44:43 +0100

    JUnit start
    - Moved to using Eclipse rather than a text editor.
    - Commented out the method as I believed it would be easier to test

---
+++ fizzbuzz/stage1/Stage1.java
@@ -0,0 +1,62 @@
+package fizzbuzz.stage1;
+
+class Stage1
+{
+	//Commented out the main method as i thought it would be easier to test
+	/*public static void main(String[] args)
+	{
+		String hi = evaluate(Integer.valueOf(args[0]));
+		String s = "";
+		
+		for(int i  = 1; i < 101; ++i)
+		{
+			boolean fizz = (i % 3) == 0;
+			boolean buzz = (i % 5) == 0;
+			
+			if(fizz && !buzz)
+			{
+				s = s + "fizz\n"; 
+			}
+			else if(!fizz && buzz)
+			{
+				s = s + "buzz\n";				
+			}
+			else if(fizz && buzz)
+			{
+				s = s + "fizzbuzz\n";
+			}
+			else
+			{
+				s = s + i + "\n"; 
+			}
+		}
+		
+		System.out.println(s);
+	}*/
+	
+	// new method takes in a number and outputs a string (fizz, buzz, fizzbuzz or the number itself)
+	public static String evaluate(int num)
+	{
+		String s = "";
+		boolean fizz = (num % 3) == 0;
+		boolean buzz = (num % 5) == 0;
+			
+		if(fizz && !buzz)
+		{
+			s = s + "fizz"; 
+		}
+		else if(!fizz && buzz)
+		{
+			s = s + "buzz";				
+		}
+		else if(fizz && buzz)
+		{
+			s = s + "fizzbuzz";
+		}
+		else
+		{
+			s = s + num; 
+		}
+		return s;
+	}
+}
